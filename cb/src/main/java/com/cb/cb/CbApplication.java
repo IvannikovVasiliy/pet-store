558ed6e91255d4595b45b82cc7de05df89f99020
IvannikovVasiliy <ivannikovvasiliya2001@gmail.com>
2022-09-07 14:56:09 +0300

    create module cb

---
+++ cb/src/main/java/com/cb/cb/CbApplication.java
@@ -0,0 +1,13 @@
+package com.cb.cb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CbApplication.class, args);
+    }
+
+}
